abdba53bc4ebc73bb11bce2ac458f7f5b23a05c4
Bruno Medeiros Lima <brunomlima80@gmail.com>
2023-06-24 02:10:50 -0300

    Incluir documento e PegadinhaIconeNaoFunciona.ino implementação de função pisca_leg

--- PegadinhaIconeNaoFunciona/PegadinhaIconeNaoFunciona.ino
+++ PegadinhaIconeNaoFunciona/PegadinhaIconeNaoFunciona.ino
@@ -1,52 +1,52 @@
 #include "DigiKeyboardPtBr.h"
 void setup() {
+  pinMode(0,OUTPUT); //LED on model B
+  pinMode(1,OUTPUT); //LED on model A
 }
 
 void loop() {  
-  digitalWrite(1, HIGH); // LED on <--> Action start
+  pisca_led(100);
+  DigiKeyboardPtBr.delay(100);
   DigiKeyboardPtBr.sendKeyStroke(0);
   DigiKeyboardPtBr.sendKeyStroke(KEY_D, MOD_GUI_LEFT);
-  DigiKeyboardPtBr.delay(500);
+  DigiKeyboardPtBr.delay(700);
   DigiKeyboardPtBr.sendKeyStroke(70, MOD_ALT_LEFT);
-  DigiKeyboardPtBr.delay(500);
+  DigiKeyboardPtBr.delay(700);
   DigiKeyboardPtBr.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboardPtBr.delay(500);
+  DigiKeyboardPtBr.delay(700);
   DigiKeyboardPtBr.print("mspaint");
   DigiKeyboardPtBr.sendKeyStroke(KEY_ENTER);
-  DigiKeyboardPtBr.delay(800);
+  DigiKeyboardPtBr.delay(700);
   DigiKeyboardPtBr.sendKeyStroke(KEY_V, MOD_CONTROL_LEFT);
-  DigiKeyboardPtBr.delay(500);
+  DigiKeyboardPtBr.delay(700);
   DigiKeyboardPtBr.sendKeyStroke(KEY_S, MOD_CONTROL_LEFT);
-  DigiKeyboardPtBr.delay(600);
+  DigiKeyboardPtBr.delay(700);
   DigiKeyboardPtBr.print("%USERPROFILE%\\wall.jpg");
   DigiKeyboardPtBr.sendKeyStroke(KEY_ENTER);
-  DigiKeyboardPtBr.delay(600);
+  DigiKeyboardPtBr.delay(700);
   DigiKeyboardPtBr.sendKeyStroke(KEY_F, MOD_ALT_LEFT);
-  DigiKeyboardPtBr.delay(500);
-  DigiKeyboardPtBr.sendKeyStroke(KEY_K);
-  DigiKeyboardPtBr.delay(500);
+  DigiKeyboardPtBr.delay(700);
   DigiKeyboardPtBr.sendKeyStroke(KEY_F);
-  DigiKeyboardPtBr.delay(500);
-  DigiKeyboardPtBr.sendKeyStroke(KEY_F, MOD_ALT_LEFT);
-  DigiKeyboardPtBr.delay(500);
-  DigiKeyboardPtBr.sendKeyStroke(KEY_X);
-  DigiKeyboardPtBr.delay(600);
+  DigiKeyboardPtBr.delay(700);
   DigiKeyboardPtBr.sendKeyStroke(KEY_D, MOD_GUI_LEFT);
-  DigiKeyboardPtBr.delay(500);
+  DigiKeyboardPtBr.delay(700);
   DigiKeyboardPtBr.sendKeyStroke(KEY_F10, MOD_SHIFT_LEFT);
-  DigiKeyboardPtBr.delay(500);
-  DigiKeyboardPtBr.sendKeyStroke(KEY_V);
-  DigiKeyboardPtBr.delay(200);
-  DigiKeyboardPtBr.sendKeyStroke(81);
-  DigiKeyboardPtBr.delay(200);
-  DigiKeyboardPtBr.sendKeyStroke(81);
-  DigiKeyboardPtBr.delay(200);
-  DigiKeyboardPtBr.sendKeyStroke(81);
-  DigiKeyboardPtBr.delay(200);
-  DigiKeyboardPtBr.sendKeyStroke(81);
-  DigiKeyboardPtBr.delay(200);
-  DigiKeyboardPtBr.sendKeyStroke(81);
+  DigiKeyboardPtBr.delay(700);
+  DigiKeyboardPtBr.sendKeyStroke(KEY_X);
+  DigiKeyboardPtBr.delay(700);
+  DigiKeyboardPtBr.sendKeyStroke(KEY_B);
   DigiKeyboardPtBr.sendKeyStroke(KEY_ENTER);
-  digitalWrite(1, LOW); // LED off <--> Action end
+  pisca_led(1000); 
   for(;;){ /*empty*/ }
+}
+
+void pisca_led(int velocidade){  
+  for (int inicio =1; inicio <= 10; inicio ++) {
+    digitalWrite(0, HIGH); // LED on <--> Action start
+    digitalWrite(1, HIGH); // LED on <--> Action start
+    DigiKeyboardPtBr.delay(velocidade);  
+    digitalWrite(0, LOW); // LED off <--> Action end
+    digitalWrite(1, LOW); // LED off <--> Action end
+    DigiKeyboardPtBr.delay(velocidade);  
+  } 
 }
